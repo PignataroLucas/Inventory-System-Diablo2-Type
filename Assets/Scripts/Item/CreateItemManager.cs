c17e9ec7c17888e9196e88d7f0137fc373531914
PignataroLucas <91335382+PignataroLucas@users.noreply.github.com>
2023-03-26 17:00:10 -0300

    Object Pool script

--- Assets/Scripts/Item/CreateItemManager.cs
+++ Assets/Scripts/Item/CreateItemManager.cs
@@ -4,6 +4,27 @@ namespace Item
 {
     public class CreateItemManager : MonoBehaviour
     {
+        public bool willAddToList = false;
+        
+
+
+        public void RandomItem()
+        {
+            if (InventoryItemInteraction.selectedItem == null)
+            {
+                GameItem newItem = new GameItem();
+                GameItem.SetItemValues(newItem,Random.Range(0,3),Random.Range(1,101),Random.Range(0,4));
+                SpawnOrAddItem(newItem);
+            }    
+        }
+        private void SpawnOrAddItem(GameItem item)
+        {
+            if (willAddToList == false)
+            {
+                //IMPLEMENTAR!!!!!!---
+                //GameObject itemObject = itemEquipPool.GetObject();
+            }
+        }
         
     }
 }
---
+++ Assets/Scripts/Utility/ObjectPoolManager.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+namespace Utility
+{
+    public class ObjectPoolManager : MonoBehaviour
+    {
+        public GameObject prefab;
+        private Stack<GameObject> _inactiveInstances = new Stack<GameObject>();
+
+        public GameObject GetObject()
+        {
+            GameObject spawnedGameObject;
+            if (_inactiveInstances.Count > 0)
+            {
+                spawnedGameObject = _inactiveInstances.Pop();
+            }
+            else
+            {
+                spawnedGameObject = Instantiate(prefab);
+
+                PooledObject pooledObject = spawnedGameObject.AddComponent<PooledObject>();
+                pooledObject.pool = this;
+            }
+            spawnedGameObject.transform.SetParent(null);
+            spawnedGameObject.SetActive(true);
+
+            return spawnedGameObject;
+        }
+        public void ReturnObject(GameObject toReturn)
+        {
+            PooledObject pooledObject = toReturn.GetComponent<PooledObject>();
+
+            if (pooledObject != null && pooledObject.pool == this)
+            {
+                toReturn.transform.SetParent(transform);
+                toReturn.transform.position = this.transform.position;
+                toReturn.SetActive(false);
+                
+                _inactiveInstances.Push(toReturn);
+            }
+            else
+            {
+                Debug.LogWarning(toReturn.name + "Was returned to a pool it wasn't spawned from.");
+                Destroy(toReturn);
+            }
+        }
+    }
+    public class PooledObject : MonoBehaviour
+    {
+        public ObjectPoolManager pool;
+    }
+    
+}
+
+
