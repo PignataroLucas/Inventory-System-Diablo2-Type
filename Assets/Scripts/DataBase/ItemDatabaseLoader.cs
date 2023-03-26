71c3a78a5c57e088e07ac75b503a0f41a0767014
PignataroLucas <91335382+PignataroLucas@users.noreply.github.com>
2023-03-26 16:31:17 -0300

    Update ItemDatabaseLoader.cs

--- Assets/Scripts/DataBase/ItemDatabaseLoader.cs
+++ Assets/Scripts/DataBase/ItemDatabaseLoader.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.PlayerLoop;
 using Utility;
 
 namespace DataBase
@@ -33,7 +32,32 @@ namespace DataBase
         
         private void LoadDB(TextAsset textAsset)
         {
-            //string [][] grid =     
+            string[][] grid = CsvFileHandler.LoadTextFile(textAsset);
+            for (int i = 1; i < grid.Length; i++)
+            {
+                ItemData row = new ItemData();
+                row.globalID = Int32.Parse(grid[i][0]);
+                row.categoryID = Int32.Parse(grid[i][1]);
+                row.categoryName = grid[i][2];
+                row.typeID = Int32.Parse(grid[i][3]);
+                row.typeName = grid[i][4];
+                typeNameList.Add(row.typeName);
+                row.size = new IntVector2(Int32.Parse(grid[i][5]), Int32.Parse(grid[i][6]));
+                row.icon = Resources.Load<Sprite>("itemIcons/" + grid[i][4]);
+                dbList.Add(row);
+            }
         }
+        
+        public void PassItemData(ref GameItem item)
+        {
+            int id = item.globalID;
+            item.categoryID = dbList[id].categoryID;
+            item.categoryName = dbList[id].categoryName;
+            item.typeID = dbList[id].typeID;
+            item.typeName = dbList[id].typeName;
+            item.size = dbList[id].size;
+            item.icon = dbList[id].icon;
+        }
+
     }
 }
